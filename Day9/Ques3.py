0abe6948452cdb912c4500ec6a25fcc67a13a094
Kashish Agrawal <72383661+kashish-ag@users.noreply.github.com>
2021-04-10 21:43:52 +0530

    Ques3
    Topic: Array
    Level: easy

---
+++ Day9/Ques3.py
@@ -0,0 +1,14 @@
+#Question Link: https://leetcode.com/problems/check-if-two-string-arrays-are-equivalent/
+
+class Solution:
+    def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
+        str1=""
+        str2=""
+        for i in word1:
+            str1=str1+i
+        for i in word2:
+            str2=str2+i
+        if (str1==str2):
+            return True
+        else:
+            return False
