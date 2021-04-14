2e0cccd3e92cc23a1c4056dc94fc01f5604bf405
Kashish Agrawal <72383661+kashish-ag@users.noreply.github.com>
2021-04-14 22:06:47 +0530

    Ques1
    Topic: Array
    Level : easy

---
+++ Day 14/Ques1.py
@@ -0,0 +1,11 @@
+#Question Link:https://leetcode.com/problems/final-prices-with-a-special-discount-in-a-shop/ 
+
+class Solution:
+    def finalPrices(self, prices: List[int]) -> List[int]:
+        n=len(prices)
+        for i in range(n-1):
+            for j in range(i+1,n):
+                if prices[j] <= prices[i]:
+                    prices[i]-=prices[j]
+                    break
+        return prices
