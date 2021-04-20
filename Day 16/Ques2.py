a702fb6bb7b1815baef9e7ceb550efe34edd8d1c
Kashish Agrawal <72383661+kashish-ag@users.noreply.github.com>
2021-04-20 21:31:06 +0530

    Ques2.py

---
+++ Day 16/Ques2.py
@@ -0,0 +1,8 @@
+#Question Link:https://leetcode.com/problems/sum-of-all-odd-length-subarrays/
+class Solution:
+    def sumOddLengthSubarrays(self, arr: List[int]) -> int:
+        s=0
+        for i in range(len(arr)):
+            for j in range(i,len(arr),2):
+                s+=sum(arr[i:j+1])
+        return s
